1837946a33ada419404760f27662801341af4f9a
Sambhav Satija <sambhav13085@iiitd.ac.in>
2015-08-26 08:45:06 +0530

    Added a function which reads the template from the file and renders it.

--- server.py
+++ server.py
@@ -1,8 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route("/")
+def counter_home_page():
+    return render_template("home.html")
+
+
+@app.route("/useless")
 def handle():
     return "Doesn't matter if the work's incomplete."
 
