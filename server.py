5bfe415329a4f5aa5a832a05a0ec818e64febdd7
Sambhav Satija <sambhav13085@iiitd.ac.in>
2015-08-26 09:13:44 +0530

    Added a danger link. Increment a counter whenever this button is pressed

--- server.py
+++ server.py
@@ -1,15 +1,23 @@
 from flask import Flask, render_template
 app = Flask(__name__)
-app.my_vars = {'page_count': 0}
+app.my_vars = {'page_count': 0, 'button_count': 0}
 
 
 @app.route("/")
 def counter_home_page():
     app.my_vars['page_count'] += 1
     count = app.my_vars['page_count']
+    button_count = app.my_vars['button_count']
     return render_template("home.html",
                            count_variable_visible_in_template=count,
-                           i_trust_you=False)
+                           i_trust_you=False,
+                           button_count=button_count)
+
+
+@app.route("/increment")
+def button_pressed():
+    app.my_vars['button_count'] += 1
+    return counter_home_page()
 
 
 @app.route("/useless")
