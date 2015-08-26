71c542c0f94b9a44213814f7631fdd5a77bb6261
Sambhav Satija <sambhav13085@iiitd.ac.in>
2015-08-26 08:53:23 +0530

    Added a counter for how many times the page is opened. Showed how to render a variable in the template.

--- server.py
+++ server.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
 app = Flask(__name__)
+app.my_vars = {'page_count': 0}
 
 
 @app.route("/")
 def counter_home_page():
-    return render_template("home.html")
+    app.my_vars['page_count'] += 1
+    count = app.my_vars['page_count']
+    return render_template("home.html",
+                           count_variable_visible_in_template=count)
 
 
 @app.route("/useless")
